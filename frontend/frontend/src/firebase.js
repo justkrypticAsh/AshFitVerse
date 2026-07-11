5db68edebe45024e93b8331bc01dd36649352ea2
Ashish Kumar Sharma <ashish.24scse1010897@galgotiasuniversity.ac.in>
2026-07-11 21:55:23 +0530

    add gitignore and move config to env vars

--- frontend/frontend/src/firebase.js
+++ frontend/frontend/src/firebase.js
@@ -11,12 +11,12 @@ import {
 import { getFirestore } from "firebase/firestore"; 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDRMKSQpsFN3moNMzbtW9HT6QzChZT1IdA",
-  authDomain: "ashfitverse.firebaseapp.com",
-  projectId: "ashfitverse",
-  storageBucket: "ashfitverse.firebasestorage.app",
-  messagingSenderId: "1005404951754",
-  appId: "1:1005404951754:web:505d4e79ee50566c075033",
+  apiKey:            import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain:        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId:         import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket:     import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId:             import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
